79e4a4f69aa3cbd610aadf756dc8821c7607b1bd
eyan4126-dev <eyan4126@gmail.com>
2026-04-30 21:54:33 -0300

    view de erro de login

--- app/Views/erro.php
+++ app/Views/erro.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Erro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+
+    <div class="box">
+
+        <?php
+
+        echo "Login ou senha inválidos.";
+        echo '<br>';
+        echo '<br>';
+        echo '<a href="index.php">Voltar página</a>';
+
+        ?>
+
+    </div>
+
 </body>
+
 </html>
